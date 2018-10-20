e3740aef49ca4e3c1f9c74c5576a2807de2f4452
Kunmo Kim <kunmok@berkeley.edu>
2018-10-20 00:35:31 -0700

    convolutional coding + puncturing is working. tail-bitting is in progress

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # coding_theory
 coding theory practice
+
+for EE 290C project 
+- convolutional coding 
+  - puncturing 
+  - tail-biting
+- viterbi decoder 
+- turbo encoder / decoder 
+
--- tail_biting_convolutional_code.py
+++ tail_biting_convolutional_code.py
@@ -15,38 +15,18 @@ puncturing_en = False
 puncturing_matrix = np.array([[1, 1, 0], [1, 0, 1]])
 
 data_length = 4
-dout = np.zeros(data_length+(K-1))
+dout = np.zeros(data_length)
 
-# start_state = np.uint32(0xACE1)
-start_state = np.uint32(0xF7FFFFFF)
-lfsr = np.uint32(start_state)
-for x in range(data_length):
-    newbit = np.uint32(((lfsr >> 30) ^ (lfsr >> 27)) & 0x1)
-    lfsr = (lfsr << 1) | newbit
-    lfsr = lfsr & 0x7FFFFFFF
-    if newbit > 0.5:
-        dout[x] = 1
-    else:
-        dout[x] = 0
-
-# plt.plot(dout)
-# plt.show()
-
-# two different methods: put the data into the window
-# or move the sliding window
-
-# important question : how to create a state machine diagram using p0,p1,... ?
-# dout = np.array([1, 0, 1, 1, 0, 0, 0, 0], dtype=np.bool)
-# dout = np.array([1, 1, 1, 1, 1, 1, 0, 0], dtype=np.bool)
-dout = np.array([1, 0, 1, 1, 0, 0, 0], dtype=np.bool)
+dout = np.array([1, 0, 1, 1], dtype=np.bool)
 print(dout)
-shift_registers = np.zeros(K, dtype=np.bool)
+# shift_registers = np.zeros(K, dtype=np.bool)
+shift_registers = np.array([1, 1, 0, 1])
 
 # including zero padding due to zero termination
-cc_out = np.zeros((r, data_length + (K-1)))
+cc_out = np.zeros((r, data_length))
 
 # convolutional coding
-for idx in range(data_length + (K-1)):
+for idx in range(data_length):
     for mem in reversed(range(K)):
         shift_registers[mem] = shift_registers[mem-1]
     shift_registers[0] = dout[idx]
@@ -59,12 +39,12 @@ print(cc_out[0])
 print(cc_out[1])
 
 # puncturing
-serialized_out = np.zeros(r * (data_length + (K-1)))
+serialized_out = np.zeros(r * data_length)
 cnt = 0
 nrow_pmat, ncol_pmat = puncturing_matrix.shape
 mark = 0
 puncturing_per_output = nrow_pmat * ncol_pmat / r
-for idx in range(data_length + (K-1)):
+for idx in range(data_length):
     for jdx in range(r):
         if puncturing_en:
             if puncturing_matrix[jdx][mark] > 0:
@@ -78,4 +58,3 @@ for idx in range(data_length + (K-1)):
 
 
 print(serialized_out)
-
